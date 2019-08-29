0e99df0fc6a526fc5a80659c35163bbf36b5880e
Jereme <michael@jeremehancock.com>
2019-08-29 07:37:09 -0500

    added trump

--- js/bouncing-trump.js
+++ js/bouncing-trump.js
@@ -9,7 +9,7 @@
 // The function gets called when the window is fully loaded
 window.onload = function() {
     // Get the canvas and context
-    var total_trumps = 14;
+    var total_trumps = 15;
     var canvas = document.getElementById("trump-canvas");
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
