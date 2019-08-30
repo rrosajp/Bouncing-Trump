cbf9be472a17b544bed6172e8f534824e2db91d6
Jereme <michael@jeremehancock.com>
2019-08-30 13:51:59 -0500

    Refactored some code into functions. Updated functions to take more options. Added mouseevents. Now when you click a Trump head it disappears and two more take its place.

--- js/bouncing-trump.js
+++ js/bouncing-trump.js
@@ -7,19 +7,19 @@
 // --------------------------------------------------------------------------
 
 // The function gets called when the window is fully loaded
-window.onload = function() {
+window.onload = function () {
     // Get the canvas and context
     var total_trumps = 16;
     var canvas = document.getElementById("trump-canvas");
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
     var context = canvas.getContext("2d");
-    
+
     // Timing and frames
     var lastframe = 0;
 
     var initialized = false;
-    
+
     // Level properties
     var level = {
         x: 1,
@@ -27,9 +27,9 @@ window.onload = function() {
         width: canvas.width,
         height: canvas.height
     };
-    
+
     // Define an entity class
-    var Entity = function(image, x, y, width, height, xdir, ydir, speed) {
+    var Entity = function (image, x, y, width, height, xdir, ydir, speed, filepath) {
         this.image = image;
         this.x = x;
         this.y = y;
@@ -38,32 +38,33 @@ window.onload = function() {
         this.xdir = xdir;
         this.ydir = ydir;
         this.speed = speed;
+        this.filepath = filepath;
     };
-    
+
     // Array of entities
     var entities = [];
-    
+
     // Images
     var images = [];
-    
+
     // Image loading global variables
     var loadcount = 0;
     var loadtotal = 0;
     var preloaded = false;
-    
+
     // Load images
     function loadImages(imagefiles) {
         // Initialize variables
         loadcount = 0;
         loadtotal = imagefiles.length;
         preloaded = false;
-        
+
         // Load the images
         var loadedimages = [];
-        for (var i=0; i<imagefiles.length; i++) {
+        for (var i = 0; i < imagefiles.length; i++) {
             // Create the image object
             var image = new Image();
-            
+
             // Add onload event handler
             image.onload = function () {
                 loadcount++;
@@ -72,54 +73,60 @@ window.onload = function() {
                     preloaded = true;
                 }
             };
-            
+
             // Set the source url of the image
             image.src = imagefiles[i];
-            
+
             // Save to the image array
             loadedimages[i] = image;
         }
-        
+
         // Return an array of images
         return loadedimages;
     }
 
     // Initialize
     function init(image) {
+        // Add mouse events
+        canvas.addEventListener("mousemove", onMouseMove);
+        canvas.addEventListener("mousedown", onMouseDown);
+        canvas.addEventListener("mouseup", onMouseUp);
+        canvas.addEventListener("mouseout", onMouseOut);
+
         // Add Trump
-        addTrump(image);
+        addTrump(image, 1);
 
         // Enter main loop
         main(0);
     }
 
     // Add Trump
-    function addTrump(image) {
+    function addTrump(image, number) {
         // Load images
         images = loadImages([image]);
 
-        // Create random entities
-        for (var i=0; i<1; i++) {
+        // Create entity
+        for (var i = 0; i < number; i++) {
             var scale = randRange(75, 200);
             var imageindex = i % images.length;
             var xdir = 1 - 2 * randRange(0, 1);
             var ydir = 1 - 2 * randRange(0, 1);
-            var entity = new Entity(images[imageindex], 0, 0, scale, scale, xdir, ydir, randRange(100, 100));
+            var entity = new Entity(images[imageindex], 0, 0, scale, scale, xdir, ydir, randRange(100, 100), image);
 
             // Set a random position
-            entity.x = randRange(0, level.width-entity.width);
-            entity.y = randRange(0, level.height-entity.height);
+            entity.x = randRange(0, level.width - entity.width);
+            entity.y = randRange(0, level.height - entity.height);
 
             // Add to the entities array
             entities.push(entity);
         }
     }
-    
+
     // Get a random int between low and high, inclusive
     function randRange(low, high) {
-        return Math.floor(low + Math.random()*(high-low+1));
+        return Math.floor(low + Math.random() * (high - low + 1));
     }
-    
+
     // Main loop
     function main(tframe) {
         // Request animation frames
@@ -141,20 +148,20 @@ window.onload = function() {
             render();
         }
     }
-    
+
     // Update state
     function update(tframe) {
         var dt = (tframe - lastframe) / 1000;
         lastframe = tframe;
 
         // Update entities
-        for (var i=0; i<entities.length; i++) {
+        for (var i = 0; i < entities.length; i++) {
             var entity = entities[i];
-            
+
             // Move the entity, time-based
             entity.x += dt * entity.speed * entity.xdir;
             entity.y += dt * entity.speed * entity.ydir;
-            
+
             // Handle left and right collisions with the level
             if (entity.x <= level.x) {
                 // Left edge
@@ -165,7 +172,7 @@ window.onload = function() {
                 entity.xdir = -1;
                 entity.x = level.x + level.width - entity.width;
             }
-            
+
             // Handle top and bottom collisions with the level
             if (entity.y <= level.y) {
                 // Top edge
@@ -178,63 +185,115 @@ window.onload = function() {
             }
         }
     }
-    
+
     // Render
     function render() {
         // Draw the frame
         drawFrame();
 
         // Draw the entities
-        for (var i=0; i<entities.length; i++) {
+        for (var i = 0; i < entities.length; i++) {
             var entity = entities[i];
             context.drawImage(entity.image, entity.x, entity.y, entity.width, entity.height);
         }
     }
 
+    // Reset clear Trumps and get a new one
     function reset() {
         // Clear the canvas
         entities = [];
         init(getRndTrump());
     }
-    
-    // Draw a frame with a border
+
+    // Draw a frame
     function drawFrame() {
         // Draw background
         context.fillStyle = "#363636";
         context.fillRect(0, 0, canvas.width, canvas.height);
     }
-    
+
+    // Mouse event handlers
+    function onMouseMove(e) {}
+
+    function onMouseDown(e) {
+        // Update entities
+        for (var i = 0; i < entities.length; i++) {
+            var entity = entities[i];
+
+            var pos = getMousePos(canvas, e);
+
+            // Check if we clicked the entity
+            if (pos.x >= entity.x && pos.x < entity.x + entity.width &&
+                pos.y >= entity.y && pos.y < entity.y + entity.height) {
+
+                // Remove the current Trump
+                var index = entities.indexOf(entity);
+                if (index > -1) {
+                    entities.splice(index, 1);
+                }
+
+                // Add two more of the current Trump
+                addTrump(entity.filepath, 2);
+
+                // Remove trump button toggle
+                removeTrumpToggle();
+            }
+        }
+
+    }
+
+    function onMouseUp(e) {}
+
+    function onMouseOut(e) {}
+
+    // Get the mouse position
+    function getMousePos(canvas, e) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: Math.round((e.clientX - rect.left) / (rect.right - rect.left) * canvas.width),
+            y: Math.round((e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height)
+        };
+    }
+
     // Get a random Trump
     function getRndTrump() {
         trump = "images/trump" + (Math.floor(Math.random() * (total_trumps - 1)) + 2) + ".png";
         return trump;
     }
 
-    // Add Trump
-    document.getElementById('add-trump').onclick = function(e) {
-        addTrump(getRndTrump());
+    // Toggle remove Trump button
+    function removeTrumpToggle() {
         // Show the remove trump button once there are more than 1 Trump
         if (entities.length > 1) {
             document.getElementById("remove-trump").style.visibility = "visible";
         }
+        else if (entities.length < 2) {
+            document.getElementById("remove-trump").style.visibility = "hidden";
+        }
+        else {
+            document.getElementById("remove-trump").style.visibility = "hidden";
+        }
+    }
+
+    // Add Trump
+    document.getElementById('add-trump').onclick = function (e) {
+        addTrump(getRndTrump(), 1);
+        // Remove trump button toggle
+        removeTrumpToggle();
     };
 
     // Remove Trump
-    document.getElementById('remove-trump').onclick = function(e) {
+    document.getElementById('remove-trump').onclick = function (e) {
         entities.pop();
-        // Hide the remove trump button once there is only 1 Trump
-        if (entities.length < 2) {
-            document.getElementById("remove-trump").style.visibility = "hidden";
-        }
+        // Remove trump button toggle
+        removeTrumpToggle();
     };
 
     // Reset
-    document.getElementById('reset').onclick = function(e) {
+    document.getElementById('reset').onclick = function (e) {
         reset();
-        // Hide the remove trump button once there is only 1 Trump
-        if (entities.length < 2) {
-            document.getElementById("remove-trump").style.visibility = "hidden";
-        }
+        // Remove trump button toggle
+        removeTrumpToggle();
     };
 
     // Call init to start
